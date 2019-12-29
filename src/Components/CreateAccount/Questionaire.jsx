57d42c8fe940cc1be5b8062a80f68344782b2fd3
bm-designs <james@bareminimum.site>
2019-12-29 12:15:55 -0800

    Questionaire is massively updated check it out"

--- src/Components/CreateAccount/Questionaire.jsx
+++ src/Components/CreateAccount/Questionaire.jsx
@@ -8,13 +8,37 @@ class Questionaire extends React.Component {
 	}
 	this.ReactDOM = ReactDOM;
 	this.user = props.user
+	this.currentCard = 'card-1'
 	this.nextCard = this.nextCard.bind(this)
+	this.previousCard = this.previousCard.bind(this)
 	this.update = this.update.bind(this)
+	this.finish = this.finish.bind(this)
 
 	}
-	nextCard(e){
-		console.log(e)
-		
+	finish(){
+		//log out results of the questionaire
+	}
+	previousCard(){
+		if(Number(this.currentCard.split('-')[1])>1){
+			var previousCard = 'card-'+String(Number(this.currentCard.split('-')[1])-1)
+			document.getElementById(previousCard).style.marginLeft ='';
+			var newCard = Number(this.currentCard.split('-')[1]) - 1
+			this.currentCard = 'card-'+String(newCard)
+			console.log(this.currentCard)
+		} else {
+			alert('Start of Questionaire!')
+		}
+	}
+	nextCard(){
+		//move current card then update this.currCard
+		if (Number(this.currentCard.split('-')[1])<12){
+			document.getElementById(this.currentCard).style.marginLeft ='-40vw';
+			var newCard = Number(this.currentCard.split('-')[1]) + 1
+			this.currentCard = 'card-'+String(newCard)
+		} else {
+			alert("End Of Questionaire!")
+		}
+				
 	}
 	update(e){
 		var update = e.target.value
@@ -33,24 +57,24 @@ class Questionaire extends React.Component {
 			<div id='questionaire-wrapper'>
 				<img id='questionaire-logo' src='transparent'/>
 				<div id='questionaire-container'>
-				<div id='first-card' class='questionaire-card' onMouseUp={this.nextCard.bind(this)}>
+				<div id='card-1' class='questionaire-card' onMouseUp={this.nextCard.bind(this)}>
 					<h1> How clean do you keep your living space? </h1>
 					<input name='first-preference' type='range' min='1' max='5' onInput={this.update.bind(this)} required/>
-					<input id='clean' disabled/>
+					<input id='clean' value='5' disabled/>
 				</div>
 				<div id='card-2' class='questionaire-card'>
 					<h1> Do you smoke? </h1>
-					<select>
+					<select onInput={this.nextCard}>
 						<option>No</option>
 						<option>Tobacco/Hookah</option>
 						<option>Marijuana</option>
 						<option>Other</option>
 					</select>
 				</div>
-				<div id='card-3' class='questionaire-card'>	
+				<div id='card-3' class='questionaire-card' onMouseUp={this.nextCard}>	
 					<h1> What is your relationship status?</h1>
-					<input type='radio' name='r-status' value='Single'/> Single
-					<input type='radio' name='r-status' value='Relationship'/> Relationship
+					<input type='radio' name='r-status' value='Relationship'/> Relationship<br/>
+					<input type='radio' name='r-status' value='Single'/> Single<br/>
 					<input type='radio' name='r-status' value="It's Complicated"/> It's Complicated
 				</div>
 				<div id='card-4' class='questionaire-card'>
@@ -63,48 +87,51 @@ class Questionaire extends React.Component {
 						<option> Rodent </option>
 					</select>
 					<br/>
-					<input type='number'/>
+					<input id='clean' min='0' type='number' value='0'/>
 				</div>
-				<div id='card-5' class='questionaire-card'>
+				<div id='card-5' class='questionaire-card' onMouseUp={this.nextCard}>
 					<h1>Do you snore ? </h1>
 					<input type='radio' name='snore' value='no'/> No
 					<input type='radio' name='snore' value='yes'/> Yes
 				</div>
-				<div id='card-6' class='questionaire-card'>
+				<div id='card-6' class='questionaire-card' onMouseUp={this.nextCard}>
 					<h1> Are you okay with social gatherings at your house? </h1>
-					<input type='radio' name='gatherings' value='Occasionally (Breaks/Weekends)'/>Occasionally
-					<input type='radio' name='gatherings' value='No'/> No
+					<input type='radio' name='gatherings' value='Occasionally (Breaks/Weekends)'/>Occasionally<br/>
+					<input type='radio' name='gatherings' value='No'/> No<br/>
 					<input type='radio' name='gatherings' value='Yes'/> Yes
 				</div>
 				<div id='card-7' class='questionaire-card'>
 					<h1>What time do you usually go to bed? </h1>
 					<input type='time'/>
 				</div>
-				<div id='card-8' class='questionaire-card'>
+				<div id='card-8' class='questionaire-card' onMouseUp={this.nextCard}>
 					<h1>Do you drink alcohol</h1>
 					<input type='radio' name='drink' value='yes'/> Yes
 					<input type='radio' name='drink' value='no'/> No
 				</div>
-				<div id='card-9' class='questionaire-card'>
+				<div id='card-9' class='questionaire-card' onMouseUp={this.nextCard}>
 					<h1>How often do you cook or use the kitchen?</h1>
-					<input type='radio' name='kitchen' value='barely'/> Barely
-					<input type='radio' name='kitchen' value='frequently'/> Frequently
+					<input type='radio' name='kitchen' value='barely'/> Barely<br/>
+					<input type='radio' name='kitchen' value='frequently'/> Frequently<br/>
 					<input type='radio' name='kitchen' value='everyday'/> Everyday
 				</div>
 				<div id='card-10' class='questionaire-card'>
 					<h1> What are some of your hobbies?</h1>
-					<input type='text' name='hobbies'/>
+					<textarea type='text' name='hobbies'></textarea>
 				</div>
 				<div id='card-11' class='questionaire-card'>
 					<h1> My roommates absolutely cannot....</h1>
-					<input type='text' name='nonos'/>
+					<textarea type='text' name='nonos'></textarea>
 				</div>
 				<div id='card-12' class='questionaire-card'>
 					<h1> My roommates abslutely must....</h1>
-					<input type='text' name='musts'/>
+					<textarea type='text' name='musts'></textarea>
+					<button onClick={this.finish}> Finish Questionaire </button>
 				</div>
 				</div>
-				<img src='/house' id='questionaire-create'/>
+				<button id='previous-card' onClick={this.previousCard}> ← </button>
+				<button id='next-card' onClick={this.nextCard}> → </button>
+				
 			</div>
 			)
 	}
